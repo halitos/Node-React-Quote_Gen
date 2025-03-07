88dd3bcc9f9a63d2fa1e4c86f919e5b9014e0e40
Halit <oskan96@gmail.com>
2025-03-07 14:10:46 +0000

    bring old joyful stuff up

--- src/QuoteDisplay.js
+++ src/QuoteDisplay.js
@@ -1,20 +1,50 @@
 import React, { useState, useEffect } from "react";
 
+const mockQuotes = [
+  {
+    quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  {
+    quote: "Drink Cay",
+    author: 'Halit "The Great"',
+  }
+];
+
+
 const QuoteDisplay = () => {
   const [quotes, setQuotes] = useState();
   const [nextQuote, setNextQuote] = useState();
 
+  const URL = "https://halit-quote-server.glitch.me/quotes"; // no longer works
+
+  // const fetchQuotes = async () => {
+  //   try {
+  //     const response = await fetch(URL);
+  //     const data = await response.json();
+  //     setQuotes(data);
+  //   }
+  //   catch (error) {
+  //     setQuotes(mockQuotes);
+  //     console.log(error);
+  //   }
+  // }
+
   useEffect(() => {
-    fetch(`https://halit-quote-server.glitch.me/quotes`)
-      .then((res) => res.json())
-      .then((data) => setQuotes(data));
+    // fetchQuotes();
+    setQuotes(mockQuotes);
   }, []);
 
   function pickFromArray(quotes) {
     return quotes[Math.floor(Math.random() * quotes.length)];
   }
 
-  function pickNext() {
+  function pickNext(e) {
+    e.preventDefault();
     setNextQuote(pickFromArray(quotes));
     console.log(nextQuote);
   }
@@ -22,12 +52,13 @@ const QuoteDisplay = () => {
   if (!quotes) {
     return <div className="lds-dual-ring"></div>;
   } else {
+    const selectedQuote = pickFromArray(quotes);
     return (
       <div>
         <h2>
-          <i>"{nextQuote ? nextQuote.quote : pickFromArray(quotes).quote}"</i>
+          <i>"{nextQuote ? nextQuote.quote : selectedQuote.quote}"</i>
         </h2>
-        <h4>- {nextQuote ? nextQuote.author : pickFromArray(quotes).author}</h4>
+        <h4>- {nextQuote ? nextQuote.author : selectedQuote.author}</h4>
         <button onClick={pickNext}>Next Quote</button>
       </div>
     );
